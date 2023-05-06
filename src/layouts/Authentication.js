22f947f698e758a66c16b83887f03de23c322762
Nazar.Naghmeh <Nazar.Naghmeh@Kourosh-AM.com>
2023-05-06 11:03:51 +0330

    img fixed

--- src/layouts/Authentication.js
+++ src/layouts/Authentication.js
@@ -24,7 +24,7 @@ export default function Authentication() {
                 <div
                     style={{
                         backgroundImage: "url(" + getBgImage() + ")",
-                        backgroundSize: "cover",
+                        backgroundSize: "contain",
                         height: "100vh"
                     }}
                 >
